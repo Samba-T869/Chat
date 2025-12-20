eaebe18777994588d71f90e8652843bbe690dfd0
Samba-T869 <Kimwagangalula45@gmail.com>
2025-12-21 01:19:12 +0300

    Delete public_page.py

--- public_page.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, session, render_template, redirect, url_for
-
-public_bp = Blueprint('public_bp', __name__)
-
-@public_bp.route('/public')
-def public():
-	if 'user' not in session:
-		return redirect(url_for('load'))
-	return render_template('public.html')
